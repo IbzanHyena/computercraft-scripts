d54108655cbc4913877a431e164e834878cbf476
Ibzan <ibzan@ibzan.co.uk>
2025-03-24 00:53:23 +0000

    Support length = 1 or width = 1 for lwdexcavate

--- lwdexcavate.lua
+++ lwdexcavate.lua
@@ -10,13 +10,13 @@ if #argv ~= 3 then
 end
 
 Length = tonumber(argv[1])
-if Length == nil or Length < 2 then
+if Length == nil or Length < 1 then
     print("Length must be positive.")
     return
 end
 
 Width = tonumber(argv[2])
-if Width == nil or Width < 2 then
+if Width == nil or Width < 1 then
     print("Width must be positive.")
     return
 end
@@ -26,8 +26,31 @@ if Depth == nil or Depth < 1 then
     print("Depth must be positive")
 end
 
-for i = 1,Depth do
-    extraTurtle.walkGrid(Length, Width, turtle.digDown)
-    extraTurtle.refuelToMin(1)
-    extraTurtle.tolerantMove("down")
+if Length == 1 then
+    turtle.turnRight()
+    for i = 1,Depth do
+        for i = 1,Width do
+            turtle.digDown()
+            extraTurtle.tolerantMove("forward")
+        end
+        turtle.turnRight()
+        turtle.turnRight()
+        extraTurtle.tolerantMove("down")
+    end
+elseif Width == 1 then
+    for i = 1,Depth do
+        for i = 1,Length do
+            turtle.digDown()
+            extraTurtle.tolerantMove("forward")
+        end
+        turtle.turnRight()
+        turtle.turnRight()
+        extraTurtle.tolerantMove("down")
+    end
+else
+    for i = 1,Depth do
+        extraTurtle.walkGrid(Length, Width, turtle.digDown)
+        extraTurtle.refuelToMin(1)
+        extraTurtle.tolerantMove("down")
+    end
 end
