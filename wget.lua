2c4a3f3f67763eb69bf35ba6178a45608590aec2
Ibzan <ibzan@ibzan.co.uk>
2025-04-12 11:18:20 +0100

    Add wget

---
+++ wget.lua
@@ -0,0 +1,28 @@
+local argv = { ... }
+
+if #argv ~= 2 then
+    print("Usage: wget <url> <target>")
+end
+
+local url = argv[1]
+local target = argv[2]
+
+if not http then
+    print("http not enabled")
+    return
+end
+
+local response = http.get(textutils.urlEncode(url))
+if not response then
+    print("failed to connect")
+    return
+end
+
+local file = fs.open(target, "w")
+if not file then
+    print("failed to open file")
+    return
+end
+
+file.write(response.readAll())
+file.close()
