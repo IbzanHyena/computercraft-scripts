4120f8ce241945f2c175e483fd3bae5320fae9c3
Ibzan <ibzan@ibzan.co.uk>
2025-04-14 22:58:55 +0100

    First attempt at multi-layer lwdexcavate

--- lwdexcavate.lua
+++ lwdexcavate.lua
@@ -24,6 +24,19 @@ end
 Depth = tonumber(argv[3])
 if Depth == nil or Depth < 1 then
     print("Depth must be positive")
+    return
+end
+
+local digFunctions = {
+    turtle.digDown,
+    turtle.dig,
+    turtle.digUp,
+}
+
+local function dig(n)
+    for i = 1,n do
+        digFunctions[i]()
+    end
 end
 
 local state = {}
@@ -31,43 +44,47 @@ local digLayer
 
 if Length == 1 and Depth == 1 then
     extraTurtle.refuelToMin(Depth)
-    digLayer = function()
+    digLayer = function (toDig)
+        -- regardless of what toDig is, we can only dig one block down
         turtle.digDown()
+        return 1
     end
 elseif Length == 1 then
     turtle.turnRight()
-    digLayer = function ()
+    digLayer = function (toDig)
         extraTurtle.refuelToMin(Width)
         for i = 1,Width-1 do
-            turtle.digDown()
+            dig(toDig)
             extraTurtle.tolerantMove("forward")
         end
         turtle.digDown()
         turtle.turnRight()
         turtle.turnRight()
+        return toDig
     end
 elseif Width == 1 then
     digLayer = function ()
         extraTurtle.refuelToMin(Length)
         for i = 1,Length-1 do
-            turtle.digDown()
+            dig(toDig)
             extraTurtle.tolerantMove("forward")
         end
         turtle.digDown()
         turtle.turnRight()
         turtle.turnRight()
+        return toDig
     end
 elseif (Length % 2 == 0) or (Width % 2 == 0) then
     local path = extraTurtle.GridHamiltonianCycle:new(Length, Width)
-    digLayer = function ()
+    digLayer = function (toDig)
         extraTurtle.refuelToMin(Length * Width)
-        path:walk(turtle.digDown)
+        path:walk(function () dig(toDig) end)
     end
 else
     local path = extraTurtle.GridPath:new(Length, Width)
-    digLayer = function (s)
+    digLayer = function (toDig, s)
         extraTurtle.refuelToMin(Length * Width)
-        path:walk(turtle.digDown, s.inReverse)
+        path:walk(function () dig(toDig) end, s.inReverse)
         turtle.digDown()
         turtle.turnRight()
         turtle.turnRight()
@@ -77,15 +94,41 @@ else
 end
 
 local startTime = os.clock()
-for i = 1,Depth do
-    digLayer(state)
+local remaining, completed = Depth, 0
+local first = true
+while remaining > 0 do
+    -- dig up to three layers: below, equal, above (in that order)
+    local toDig = math.min(remaining, 3)
+    if toDig == 2 then
+        -- we must move down an extra block so that the equal level is filled
+        extraTurtle.refuelToMin(1)
+        extraTurtle.digDown()
+        extraTurtle.tolerantMove("down")
+    elseif toDig == 3 then
+        -- we must move down two extra blocks so that equal and above levels are filled
+        extraTurtle.refuelToMin(2)
+        extraTurtle.digDown()
+        extraTurtle.tolerantMove("down")
+        extraTurtle.digDown()
+        extraTurtle.tolerantMove("down")
+    end
+    local dug = digLayer(toDig, state)
+    remaining = remaining - dug
+    completed = completed + dug
+
+    -- we always move down by 1 because the turtle can only dig out one block below it
     extraTurtle.refuelToMin(1)
     extraTurtle.tolerantMove("down")
+
     local now = os.clock()
-    if i > 1 then
+    if not first then
         print("----------")
+    else
+        first = false
     end
-    print("Completed layer " .. i .. "/" .. Depth)
-    print("Elapsed time: " .. (now - startTime) .. " s (" .. ((now - startTime) / i) .. " s/layer)")
-    print("Estimated remaining time: " .. ((now - startTime) / i) * (Depth - i) .. " s")
+
+    local elapsedTimePerLayer = (now - startTime) / completed
+    print("Completed layer " .. completed .. "/" .. Depth)
+    print("Elapsed time: " .. (now - startTime) .. " s (" .. elapsedTimePerLayer .. " s/layer)")
+    print("Estimated remaining time: " .. elapsedTimePerLayer * remaining .. " s")
 end
