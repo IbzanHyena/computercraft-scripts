ef136bb84de4ffea3623c48a965b187454e07478
Ibzan <ibzan@ibzan.co.uk>
2025-04-14 23:39:54 +0100

    Refine drop-off condition

--- bloodaltar.lua
+++ bloodaltar.lua
@@ -94,7 +94,7 @@ while true do
     -- drop off any excess output items
     select(isOutput)
     local count = turtle.getItemCount()
-    if count > 63 then
+    if count > 64 - Concurrency then
         turtle.turnLeft()
         turtle.drop(count - 1)
         turtle.turnRight()
