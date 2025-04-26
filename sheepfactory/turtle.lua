f251d7b0f94d1ae8a9bf773f645d9723d13ede32
Ibzan <ibzan@ibzan.co.uk>
2025-04-26 12:24:47 +0100

    Redstone to trigger main program

--- sheepfactory/turtle.lua
+++ sheepfactory/turtle.lua
@@ -2,21 +2,12 @@ if not os.loadAPI("/apis/clientserver") then
     print("Failed to load clientserver API")
 end
 
-local aspectalyzer = peripheral.wrap("front")
-
 local quotaAspects = {"corpus", "humanus", "terra", "vinculum"}
-local quota = {}
-
-for _, v in ipairs(quotaAspects) do
-    quota[v] = math.random(8, 32)
-end
-
-local progress = {}
-
 local quotaUpdateFrequency = 30
 local quotaUpdateAmount = 5
 local quotaUpdateProbability = 0.5
 
+local aspectalyzer = peripheral.wrap("front")
 rednet.open("left")
 rednet.host("sheepfactory", "turtle")
 clientserver.WaitForReceivers(
@@ -27,77 +18,96 @@ local doorId = rednet.lookup("sheepfactory", "door")
 local sheepDisplayId = rednet.lookup("sheepfactory", "sheepdisplay")
 local yeenDisplayId = rednet.lookup("sheepfactory", "yeendisplay")
 
-local quotaUpdateTime = os.clock()
-local lastProgressTime = os.clock()
 
+local function main()
+    local quota = {}
 
-local function calculateRelativeProgress()
-    local relative = {}
-    local finished = true
-    for k, v in pairs(quota) do
-        relative[k] = math.min((progress[k] or 0) / v, 1)
-        finished = finished and relative[k] == 1
+    for _, v in ipairs(quotaAspects) do
+        quota[v] = math.random(8, 32)
     end
-    return relative, finished
-end
+
+    local progress = {}
+    local quotaUpdateTime = os.clock()
+    local lastProgressTime = os.clock()
+
+    local function calculateRelativeProgress()
+        local relative = {}
+        local finished = true
+        for k, v in pairs(quota) do
+            relative[k] = math.min((progress[k] or 0) / v, 1)
+            finished = finished and relative[k] == 1
+        end
+        return relative, finished
+    end
 
 
-local function updateDisplays()
-    local relative, finished = calculateRelativeProgress()
-    local data = {quota=quota, progress=progress, relative=relative}
-    rednet.send(sheepDisplayId, data, "sheepfactory")
-    rednet.send(yeenDisplayId, data, "sheepfactory")
-    return finished
-end
+    local function updateDisplays()
+        local relative, finished = calculateRelativeProgress()
+        local data = {quota=quota, progress=progress, relative=relative}
+        rednet.send(sheepDisplayId, data, "sheepfactory")
+        rednet.send(yeenDisplayId, data, "sheepfactory")
+        return finished
+    end
 
 
-local function increaseQuota()
-    while true do
-        if
-            (os.clock() - lastProgressTime) > quotaUpdateFrequency
-            and (os.clock() - quotaUpdateTime) > quotaUpdateFrequency
-        then
-            for k, v in pairs(quota) do
-                if
-                    (progress[k] or 0) < quota[k]
-                    and math.random() < quotaUpdateProbability
-                then
-                    quota[k] = v + quotaUpdateAmount
+    local function increaseQuota()
+        while true do
+            if
+                (os.clock() - lastProgressTime) > quotaUpdateFrequency
+                and (os.clock() - quotaUpdateTime) > quotaUpdateFrequency
+            then
+                for k, v in pairs(quota) do
+                    if
+                        (progress[k] or 0) < quota[k]
+                        and math.random() < quotaUpdateProbability
+                    then
+                        quota[k] = v + quotaUpdateAmount
+                    end
                 end
+                quotaUpdateTime = os.clock()
+                if updateDisplays() then return end
+            else
+                sleep(1)
             end
-            quotaUpdateTime = os.clock()
-            if updateDisplays() then return end
-        else
-            sleep(1)
         end
     end
-end
 
 
-local function readItems()
-    while true do
-        if turtle.suckUp() then
-            local count = turtle.getItemCount()
-            turtle.drop()
-            local aspects = aspectalyzer.getAspectCount()
-            for k, v in pairs(aspects) do
-                progress[k] = math.min(
-                    (progress[k] or 0) + v * count,
-                    quota[k] or math.huge
-                )
+    local function readItems()
+        while true do
+            if turtle.suckUp() then
+                local count = turtle.getItemCount()
+                turtle.drop()
+                local aspects = aspectalyzer.getAspectCount()
+                for k, v in pairs(aspects) do
+                    progress[k] = math.min(
+                        (progress[k] or 0) + v * count,
+                        quota[k] or math.huge
+                    )
+                end
+                turtle.suck()
+                turtle.dropDown()
+
+                lastProgressTime = os.clock()
+                if updateDisplays() then return end
+            else
+                sleep(1)
             end
-            turtle.suck()
-            turtle.dropDown()
-
-            lastProgressTime = os.clock()
-            if updateDisplays() then return end
-        else
-            sleep(1)
         end
     end
+
+    rednet.send(doorId, false, "sheepfactory")
+    updateDisplays()
+    parallel.waitForAny(increaseQuota, readItems)
+    rednet.send(doorId, true, "sheepfactory")
 end
 
-rednet.send(doorId, false, "sheepfactory")
-updateDisplays()
-parallel.waitForAny(increaseQuota, readItems)
-rednet.send(doorId, true, "sheepfactory")
+while true do
+    local signalReceived = false
+    for _, v in ipairs(redstone.getSides()) do
+        signalReceived = redstone.getInput(v)
+        if signalReceived then break end
+    end
+
+    if signalReceived then main() else sleep(0.1) end
+end
