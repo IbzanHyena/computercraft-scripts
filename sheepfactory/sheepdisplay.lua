91e1447572cce03f03043cc56f225206d2285a58
Ibzan <ibzan@ibzan.co.uk>
2025-04-26 11:41:54 +0100

    Start at top

--- sheepfactory/sheepdisplay.lua
+++ sheepfactory/sheepdisplay.lua
@@ -21,7 +21,7 @@ while true do
         monitor.setCursorPos(1, 1)
         monitor.write(message["message"])
     end
-    local y = 1
+    local y = 0
     for k, _ in pairs(message["quota"]) do
         y = y + 1
         monitor.setCursorPos(1, y)
