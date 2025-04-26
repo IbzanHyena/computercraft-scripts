2731049fda382b233ee065bf715dc1f6f100faf3
Ibzan <ibzan@ibzan.co.uk>
2025-04-26 11:53:35 +0100

    Quota increasing

--- sheepfactory/turtle.lua
+++ sheepfactory/turtle.lua
@@ -18,30 +18,64 @@ rednet.send(doorId, false, "sheepfactory")
 rednet.send(sheepDisplayId, {quota=quota, progress=progress, relative={}}, "sheepfactory")
 rednet.send(yeenDisplayId, {quota=quota, progress=progress, relative={}}, "sheepfactory")
 
-while true do
-    if turtle.suckUp() then
-        local count = turtle.getItemCount()
-        turtle.drop()
-        local aspects = aspectalyzer.getAspectCount()
-        for k, v in pairs(aspects) do
-            progress[k] = (progress[k] or 0) + v * count
-        end
-        turtle.suck()
-        turtle.dropDown()
+local quotaUpdateTime = os.clock()
+local lastProgressTime = os.clock()
 
-        local relative = {}
-        local finished = true
-        for k, v in pairs(quota) do
-            relative[k] = math.min((progress[k] or 0) / v, 1)
-            finished = finished and relative[k] == 1
+
+local function calculateRelativeProgress()
+    local relative = {}
+    local finished = true
+    for k, v in pairs(quota) do
+        relative[k] = math.min((progress[k] or 0) / v, 1)
+        finished = finished and relative[k] == 1
+    end
+    return relative, finished
+end
+
+
+local function updateDisplays()
+    local relative, finished = calculateRelativeProgress()
+    local data = {quota=quota, progress=progress, relative=relative}
+    rednet.send(sheepDisplayId, data, "sheepfactory")
+    rednet.send(yeenDisplayId, data, "sheepfactory")
+    return finished
+end
+
+
+local function increaseQuota()
+    while true do
+        if lastProgressTime > 5 then
+            for k, v in pairs(quota) do
+                quota[k] = v + 1
+            end
+            quotaUpdateTime = os.clock()
+            if updateDisplays() then return end
+        else
+            sleep(1)
         end
-        local data = {quota=quota, progress=progress, relative=relative}
-        rednet.send(sheepDisplayId, data, "sheepfactory")
-        rednet.send(yeenDisplayId, data, "sheepfactory")
-        if finished then break end
-    else
-        sleep(1)
     end
 end
 
+
+local function readItems()
+    while true do
+        if turtle.suckUp() then
+            local count = turtle.getItemCount()
+            turtle.drop()
+            local aspects = aspectalyzer.getAspectCount()
+            for k, v in pairs(aspects) do
+                progress[k] = (progress[k] or 0) + v * count
+            end
+            turtle.suck()
+            turtle.dropDown()
+
+            lastProgressTime = os.clock()
+            if updateDisplays() then return end
+        else
+            sleep(1)
+        end
+    end
+end
+
+parallel.waitForAny(increaseQuota, readItems)
 rednet.send(doorId, true, "sheepfactory")
